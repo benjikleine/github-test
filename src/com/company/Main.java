3da62e0ed7e3c45624887d6c2f5244faebe5ef86
benjamin kanon <b.kanon@novi-education.nl>
2020-12-28 09:15:55 +0100

    eerste setup

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.List;
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+        List <String> firstnames = new ArrayList<>();
+        firstnames.add("Benjamin");
+        firstnames.add("Melanie");
+        firstnames.add("Jayden");
+        firstnames.add("Henk");
+        firstnames.add("Joke");
+
+        System.out.println("De eerste persoon in de lijst is:" + firstnames.get(0gitt));
+        System.out.println("De laatste persoon in de lijst is:" + firstnames.get(4));
+
+
+    }
+}
